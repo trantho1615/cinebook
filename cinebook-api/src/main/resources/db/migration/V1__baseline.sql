f7b3a9c59f545091d142757d2503159dd8c4c6f1
trantho1615 <trantho160105@gmail.com>
2026-08-11 18:52:54 +0700

    feat: add postgres, flyway and testcontainers test harness

---
+++ cinebook-api/src/main/resources/db/migration/V1__baseline.sql
@@ -0,0 +1,3 @@
+-- btree_gist cho phep dung toan tu dang thuc (=) trong rang buoc EXCLUDE,
+-- can thiet cho rang buoc chong trung lich chieu trong cung mot phong (spec muc 5.7).
+CREATE EXTENSION IF NOT EXISTS btree_gist;
--- cinebook-api/src/test/java/com/cinebook/ApiApplicationTest.java
+++ cinebook-api/src/test/java/com/cinebook/ApiApplicationTest.java
@@ -1,5 +1,6 @@
 package com.cinebook;
 
+import com.cinebook.support.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,8 +8,13 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Ke thua AbstractIntegrationTest de dung container Postgres rieng.
+ * Neu khong, test se ket noi vao Postgres cua docker compose tren may lap trinh vien
+ * va do tren CI, noi khong co compose nao chay.
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ApiApplicationTest {
+class ApiApplicationTest extends AbstractIntegrationTest {
 
     @Autowired
     TestRestTemplate rest;
