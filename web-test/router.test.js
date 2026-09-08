b6b9e10b21c942e807d83804e080015f29c3ee64
trantho1615 <trantho160105@gmail.com>
2026-09-09 03:14:23 +0700

    test: cover thanh-toan route in router param test

--- web-test/router.test.js
+++ web-test/router.test.js
@@ -11,6 +11,7 @@ test("tuyen co tham so tra ve dung id", () => {
     assert.deepEqual(phanGiai("#/phim/abc-123"), { ten: "chiTietPhim", thamSo: { id: "abc-123" } });
     assert.deepEqual(phanGiai("#/suat/s-9/ghe"), { ten: "soDoGhe", thamSo: { id: "s-9" } });
     assert.deepEqual(phanGiai("#/ve/b-7"), { ten: "chiTietVe", thamSo: { id: "b-7" } });
+    assert.deepEqual(phanGiai("#/thanh-toan/b-42"), { ten: "thanhToan", thamSo: { id: "b-42" } });
 });
 
 test("tuyen khong tham so", () => {
